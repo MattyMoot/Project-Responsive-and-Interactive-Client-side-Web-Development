a6131d2fd8c5432b7970726cc28f705bf00205ca
MattyMoot <40363714+MattyMoot@users.noreply.github.com>
2020-01-17 15:12:12 +1300

    Add files via upload
    fixed the form

--- Project/js/App.js
+++ Project/js/App.js
@@ -13,6 +13,29 @@ class navigation extends Component {
     }
 }
 
-export default navigation 
+class nameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { Name: '', Counrty: '', description: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        console.log('Thank you for Submiting the form' + this.state.Name, 'from' + this.state.Counrty, 'your favourite place in New Zealand is:' + this.state.description)
+        event.preventDefault();
+    }
+
+    render() {
+        return ();
+    }
+}
+
+export default all 
 
 
